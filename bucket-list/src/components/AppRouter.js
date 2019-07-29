5f230dfc4222e65d6ab5321e3e715ef3a8b1d8a4
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2019-07-29 14:10:15 -0500

    created AppRouter, BucketList, CompletedList, ItemCard

---
+++ bucket-list/src/components/AppRouter.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import BucketList from "./BucketList";
+import CompletedList from "./CompletedList";
+
+export default function AppRouter() {
+  return (
+    <div className="page-view ui bottom attached segment active tab">
+      <Switch>
+        <Route path="/list" component={BucketList} />
+        <Route path="/completed" component={CompletedList} />
+      </Switch>
+    </div>
+  );
+}
---
+++ bucket-list/src/components/CompletedList.js
@@ -0,0 +1,7 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ItemCard from "./ItemCard";
+
+export default function CompletedList() {
+  return <section />;
+}
