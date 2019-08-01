427975919c7f4e83ae930fc41a733da442487fbd
Jenny Li <jli226@fordham.edu>
2019-08-01 00:25:28 -0700

    add social sharing to bucketlist page

--- bucket-list/src/components/BucketList.js
+++ bucket-list/src/components/BucketList.js
@@ -2,16 +2,30 @@ import React from "react";
 import axios from "axios";
 import ItemCard from "./ItemCard";
 
+import { Twitter } from "react-social-sharing";
+import { Facebook } from "react-social-sharing";
+import { Pinterest } from "react-social-sharing";
+import { Reddit } from "react-social-sharing";
+import { Tumblr } from "react-social-sharing";
+
 export default function BucketList(props) {
   return (
-    <section className="bucket-list">
-      {props.items.map(item => (
-        <ItemCard
-          {...props}
-          item={item}
-          deleteActiveItem={props.deleteActiveItem}
-        />
-      ))}
-    </section>
+    <>
+      <section className="bucket-list">
+        {props.items.map(item => (
+          <ItemCard
+            {...props}
+            item={item}
+            deleteActiveItem={props.deleteActiveItem}
+          />
+        ))}
+      </section>
+
+      <Twitter link="https://github.com" />
+      <Pinterest link="http://sharingbuttons.io" />
+      <Reddit link="http://sharingbuttons.io" />
+      <Tumblr link="http://sharingbuttons.io" />
+      <Facebook link="http://sharingbuttons.io" />
+    </>
   );
 }
